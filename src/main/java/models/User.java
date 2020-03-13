c7340a1ac2bafbbc1c51efdc2268d14e836a05e1
Kathleen West <2509480w@student.gla.ac.uk>
2020-03-13 19:33:06 +0000

    Adding base user class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,26 @@
 package models;
 
 public class User {
+
+    private int userId;
+    private String username;
+    private Basket basket;
+    private static int userCount = 1;
+
+    public User(String inUsername) {
+        userId = userCount;
+        username = inUsername;
+        basket = new Basket();
+
+        userCount++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
 }
