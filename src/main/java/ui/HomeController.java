291d14bd9dda32d2a0d98b065b76cccf38e80067
Kathleen West <2509480w@student.gla.ac.uk>
2020-03-13 13:20:58 +0000

    Rolling Back HomeController file

--- src/main/java/ui/HomeController.java
+++ src/main/java/ui/HomeController.java
@@ -1,27 +1,7 @@
 package ui;
 
-import javafx.event.ActionEvent;
-
-import java.io.IOException;
-
 public class HomeController {
 
-    Navigator navigator = Navigator.getInstance();
 
-    public void viewProducts(ActionEvent event) {
-        try {
-            navigator.viewProducts(event);
-        } catch (IOException e ){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void viewBasket(ActionEvent event) {
-        try {
-            navigator.viewProduct(event);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
 }
