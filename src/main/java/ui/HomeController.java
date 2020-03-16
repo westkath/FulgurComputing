d60804378a93d089f7cf133d4bd2d08df0f86f60
Robynn McCrossan <2483898m@student.gla.ac.uk>
2020-03-16 08:34:12 +0000

    updated home controller

--- src/main/java/ui/HomeController.java
+++ src/main/java/ui/HomeController.java
@@ -1,7 +1,26 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
 public class HomeController {
 
+    private Navigator navigator = Navigator.getInstance();
 
+    public void viewProducts(ActionEvent event) {
+        try {
+            navigator.viewProducts(event);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void viewBasket(ActionEvent event) {
+        try {
+            navigator.viewBasket(event);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
