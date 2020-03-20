1c88f502fa2b8a049837f4d334badf9bf88e0476
Kathleen West <2509480w@student.gla.ac.uk>
2020-03-20 22:52:22 +0000

    Refactoring DBFunctionality interface, removing exceptions - handled in DBConnection appropriately

--- src/main/java/data/DBFunctionality.java
+++ src/main/java/data/DBFunctionality.java
@@ -3,13 +3,12 @@ package data;
 import models.Product;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public interface DBFunctionality {
 
     void prepareDatabase();
-    ResultSet getProductsInTable() throws SQLException;
-    Product getProductById(int productId) throws SQLException;
+    ResultSet getProductsInTable();
+    Product getProductById(int productId);
     void adjustStockIfNoCheckout();
     void addProductToBasket(Product product);
     void removeOneFromBasket(Product product);
