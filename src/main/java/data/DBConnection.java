02251d3c4f919bde2beeff3deb01d367343cfd93
Kathleen West <2509480w@student.gla.ac.uk>
2020-03-20 22:51:31 +0000

    Refactoring DBConnection

--- src/main/java/data/DBConnection.java
+++ src/main/java/data/DBConnection.java
@@ -2,13 +2,16 @@ package data;
 
 import models.Product;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -42,6 +45,7 @@ public class DBConnection {
 
     public void setupDatabase() {
         List<String> commands = readDatabaseSetup(getProperty(SETUP_FILE));
+
         try {
             Statement statement = conn.createStatement();
             for (String command : commands) {
@@ -53,28 +57,47 @@ public class DBConnection {
         }
     }
 
-    public ResultSet getProductsInTable() throws SQLException {
-        Statement statement = conn.createStatement();
-        return statement.executeQuery(getProperty(SELECT_ALL));
+    public ResultSet getProductsInTable() {
+        return execute(getProperty(SELECT_ALL));
     }
 
-    public Product getProductById(int productId) throws SQLException {
-        Product product = null;
+    public Product getProductById(int productId) {
         String query = getProperty(PRODUCT_BY_ID).replace(REPLACE_PRODUCT_ID, String.valueOf(productId));
+        ResultSet results = execute(query);
 
-        Statement statement = conn.createStatement();
-        ResultSet results = statement.executeQuery(query);
+        return processProduct(productId, results);
+    }
 
-        while (results.next()) {
-            String name = results.getString(NAME);
-            String description = results.getString(DESCRIPTION);
-            double price = results.getDouble(PRICE);
-            int stockLevel = results.getInt(STOCK_LEVEL);
+    private ResultSet execute(String query) {
+        ResultSet results = null;
 
-            product = new Product(productId, name, description, price, stockLevel);
+        try {
+            Statement statement = conn.createStatement();
+            results = statement.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
-        return product;
+        return results;
+    }
+
+    private Product processProduct(int productId, ResultSet results) {
+        Product result = null;
+
+        try {
+            while (results.next()) {
+                String name = results.getString(NAME);
+                String description = results.getString(DESCRIPTION);
+                double price = results.getDouble(PRICE);
+                int stockLevel = results.getInt(STOCK_LEVEL);
+
+                result = new Product(productId, name, description, price, stockLevel);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
     }
 
     public void adjustStockLevel(int productId, int quantity) {
@@ -92,6 +115,7 @@ public class DBConnection {
 
     private List<String> readDatabaseSetup(String filename) {
         List<String> commands = new ArrayList<>();
+
         try (InputStream inStream = getClass().getClassLoader().getResourceAsStream(filename);
              InputStreamReader inStreamReader = new InputStreamReader(Objects.requireNonNull(inStream), StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(inStreamReader)) {
@@ -99,12 +123,12 @@ public class DBConnection {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return commands;
     }
 
     private String getProperty(String property) {
-        Properties prop = getPropertiesFile();
-        return prop.getProperty(property);
+        return getPropertiesFile().getProperty(property);
     }
 
     private Properties getPropertiesFile() {
